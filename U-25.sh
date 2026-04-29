55f87edb652cdb507c6f8dc2532df1366a06bd1f
choeunjae <jej059813@naver.com>
2026-04-29 14:54:21 +0900

    Fix U-25 script

--- U-25.sh
+++ U-25.sh
@@ -2,11 +2,10 @@
 
 echo "U-25 Check bash script"
 
-result=$(find / -type f -perm -2 2>/dev/null)
+result=$(find / -path /proc -prune -o -path /sys -prune -o -type f -perm -2 -print 2>/dev/null)
 
 if [ -z "$result" ]; then
 	echo "[SAFE]"
 else
 	echo "[VULNERABLE]"
-	echo "$result"
 fi
